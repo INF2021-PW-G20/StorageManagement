591d009f047d3a191c9e6f8be1aff9e2fc7c4cfa
jlsOsorio <joao_lso@hotmail.com>
2021-06-13 01:11:08 +0100

    datetime-local

--- PW_M2_P2/Frontend/storage_management/src/InputoutputEdit.js
+++ PW_M2_P2/Frontend/storage_management/src/InputoutputEdit.js
@@ -1,10 +1,10 @@
-import { Edit, SimpleForm, NumberInput, DateTimeInput, ReferenceInput, SelectInput} from "react-admin";
+import { Edit, SimpleForm, NumberInput, ReferenceInput, SelectInput, TextInput} from "react-admin";
 
 export const InputoutputEdit = props => (
     <Edit {...props}>
         <SimpleForm>
             <NumberInput source="id" />
-            <DateTimeInput source="date_time"/>
+            <TextInput type="datetime-local" source="date_time"/>
             <NumberInput source="quantity" />
             <NumberInput source="operation" />
             <ReferenceInput source="storage_id" reference="storages"><SelectInput optionText="name" /></ReferenceInput>
